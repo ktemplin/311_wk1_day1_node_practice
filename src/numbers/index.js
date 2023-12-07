1b27e6b969a09e37b48ce10395ab7a852067befc
Kris Templin <kris.templin@gmail.com>
2023-12-06 19:49:59 -0800

    Almost done

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,15 +1,18 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  return num % 2 == 0
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  sumNum = arr.reduce((a, b) => a+b, 0)
+  return sumNum
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
+  sumOfNums = arr.reduce((a, b) => a+b)
+  
 
 }
 
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,21 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  let banana = str.split(delim)
+  return banana
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  let paired = str.match(/.{1,2}/g ?? [])
+  return paired
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let backSplit = str.split("")
+  let holdThis = backSplit.reverse()
+  let sdrawkcab = holdThis.join("")
+  return sdrawkcab
 }
 
 module.exports = {
